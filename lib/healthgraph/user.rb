f80aa649448ae6b5c7706eb54469caa5e4c163a8
Greg Pallotta <gregpallotta@gmail.com>
2013-07-05 20:06:01 -0400

    refactor tests, add remaining URLs and userID to user class

--- lib/healthgraph/user.rb
+++ lib/healthgraph/user.rb
@@ -3,7 +3,12 @@ require 'json'
 
 module Healthgraph
   class User
-    attr_reader :access_token, :profile_url, :settings_url
+
+    attr_reader :access_token, :profile_url, :settings_url,
+        :fitness_activities_url, :strength_training_activities_url,
+        :background_activities_url, :sleep_url, :nutrition_url, :weight_url,
+        :general_measurements_url, :diabetes_url, :records_url, :team_url,
+        :change_log_url, :userID
 
     def initialize(token)
       @access_token = token
@@ -17,6 +22,7 @@ module Healthgraph
       res = JSON.parse(res.body)
       puts res
       set_urls(res)
+      @userID = res['userID']
     end
 
     protected
@@ -31,6 +37,17 @@ module Healthgraph
       def set_urls(res)
         @profile_url = res['profile']
         @settings_url = res['settings']
+        @fitness_activities_url = res['fitness_activities']
+        @strength_training_activities_url = res['strength_training_activities']
+        @background_activities_url = res['background_activities']
+        @sleep_url = res['sleep']
+        @nutrition_url = res['nutrition']
+        @weight_url = res['weight']
+        @general_measurements_url = res['general_measurements']
+        @diabetes_url = res['diabetes']
+        @records_url = res['records']
+        @team_url = res['team']
+        @change_log_url = res['change_log']
       end
 
   end
--- spec/healthgraph/user_spec.rb
+++ spec/healthgraph/user_spec.rb
@@ -2,21 +2,33 @@ require 'spec_helper'
 
 describe Healthgraph::User do
 
-  describe "user" do
+  describe "user object" do
   end
 
-  describe "fetching a user", :vcr do
+  describe "fetching a user" do
 
     # let(:user) { Healthgraph::User.new(ENV['HEALTHGRAPH_TOKEN']) }
 
-    it "has a profile url" do
+    it "has urls", :vcr do
       user = Healthgraph::User.new(ENV['HEALTHGRAPH_TOKEN'])
       expect(user.profile_url).to be_a(String)
+      expect(user.settings_url).to be_a(String)
+      expect(user.fitness_activities_url).to be_a(String)
+      expect(user.strength_training_activities_url).to be_a(String)
+      expect(user.background_activities_url).to be_a(String)
+      expect(user.sleep_url).to be_a(String)
+      expect(user.nutrition_url).to be_a(String)
+      expect(user.weight_url).to be_a(String)
+      expect(user.general_measurements_url).to be_a(String)
+      expect(user.diabetes_url).to be_a(String)
+      expect(user.records_url).to be_a(String)
+      expect(user.team_url).to be_a(String)
+      expect(user.change_log_url).to be_a(String)
     end
 
-    it "has a settings url" do
+    it "has a user id", :vcr do
       user = Healthgraph::User.new(ENV['HEALTHGRAPH_TOKEN'])
-      expect(user.settings_url).to be_a(String)
+      expect(user.userID).to be_a(Fixnum)
     end
 
   end

7973514b2399189303a8d650c18c4bbb1f49afd8
Greg Pallotta <gregpallotta@gmail.com>
2013-07-06 13:00:03 -0400

    change profile specs to reflect optional attributes

--- spec/healthgraph/profile_spec.rb
+++ spec/healthgraph/profile_spec.rb
@@ -4,13 +4,13 @@ describe Healthgraph::Profile do
   it "has profile properties", :vcr do
     user = Healthgraph::User.get(ENV['HEALTHGRAPH_TOKEN'])
     profile = user.profile
-    expect(profile.name).to be_a(String)
-    expect(profile.location).to be_a(String)
-    expect(profile.athlete_type).to be_a(String)
-    expect(profile.gender).to be_a(String)
-    expect(profile.birthday).to be_a(String)
     expect(profile.elite).to be_a(String)
     expect(profile.profile).to be_a(String)
+    expect(profile).to respond_to(:name)
+    expect(profile).to respond_to(:location)
+    expect(profile).to respond_to(:athlete_type)
+    expect(profile).to respond_to(:gender)
+    expect(profile).to respond_to(:birthday)
     expect(profile).to respond_to(:small_picture)
     expect(profile).to respond_to(:normal_picture)
     expect(profile).to respond_to(:medium_picture)
